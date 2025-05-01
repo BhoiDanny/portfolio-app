833d23b166932025b426375df5b5d5b1e845d09e
Cypherios <brownssmith899@gmail.com>
2025-05-01 16:19:39 +0000

    chore: create dashboard screen

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,17 @@
 <?php
 
+use App\Http\Controllers\Dashboard\HomeController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('welcome');
+    return Inertia::render('home');
 })->name('home');
 
+
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('dashboard', function () {
-        return Inertia::render('dashboard');
-    })->name('dashboard');
+    Route::get('/dashboard', [HomeController::class, 'index'])->name('dashboard');
 });
 
-require __DIR__.'/settings.php';
+// require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
